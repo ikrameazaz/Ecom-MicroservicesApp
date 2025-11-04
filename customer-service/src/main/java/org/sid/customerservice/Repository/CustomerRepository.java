58c3b235b34e198a3045dc2fb6c889f985bfc210
ikrame <ikrameazaz@gmail.com>
2025-11-04 19:47:22 +0100

    Rest config

--- customer-service/src/main/java/org/sid/customerservice/Repository/CustomerRepository.java
+++ customer-service/src/main/java/org/sid/customerservice/Repository/CustomerRepository.java
@@ -6,6 +6,8 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+
 }
 
 
