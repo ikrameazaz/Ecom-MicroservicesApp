98339f8d0bdd3c16c6b793ff604d2af592c74a8a
ikrame <ikrameazaz@gmail.com>
2025-11-06 20:21:41 +0100

    Dynamic routes config

---
+++ Discovery-service/src/test/java/org/sid/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package org.sid.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
