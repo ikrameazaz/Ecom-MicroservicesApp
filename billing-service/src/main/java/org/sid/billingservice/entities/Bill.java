16be5668390825d7a66dd68b9519c90cde09be3f
ikrame <ikrameazaz@gmail.com>
2025-11-17 00:22:07 +0100

    config service

--- billing-service/src/main/java/org/sid/billingservice/entities/Bill.java
+++ billing-service/src/main/java/org/sid/billingservice/entities/Bill.java
@@ -1,13 +1,11 @@
 package org.sid.billingservice.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.sid.billingservice.model.Customer;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -18,8 +16,11 @@ import java.util.List;
 public class Bill {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Date billigDate;
+    private Date billingDate;
     private Long customerId;
-    List<ProductItem> productItems = new ArrayList<>();
+    @OneToMany(mappedBy = "bill")
+    private List<ProductItem> productItems = new ArrayList<>();
+    // Stockage temporair du client non persiste
+    @Transient private Customer customer;
 }
 
--- billing-service/src/main/java/org/sid/billingservice/entities/ProductItem.java
+++ billing-service/src/main/java/org/sid/billingservice/entities/ProductItem.java
@@ -1,15 +1,25 @@
 package org.sid.billingservice.entities;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.sid.billingservice.model.Product;
 
+@Entity @Data
+@AllArgsConstructor @NoArgsConstructor
+@Builder
 public class ProductItem {
     @Id @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
     private String productId;
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private Bill bill;
     private int quantity;
-    private double price;
-
-
+    private double unitPrice;
+    @Transient
+    private Product product;
 }
