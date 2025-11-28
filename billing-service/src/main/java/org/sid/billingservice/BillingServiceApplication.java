5ac5d3a2c44eb4cc355d04270d4a4da0ed09c223
ikrame <ikrameazaz@gmail.com>
2025-11-28 12:28:58 +0100

    commit

--- billing-service/src/main/java/org/sid/billingservice/BillingServiceApplication.java
+++ billing-service/src/main/java/org/sid/billingservice/BillingServiceApplication.java
@@ -26,33 +26,33 @@ public class BillingServiceApplication {
         SpringApplication.run(BillingServiceApplication.class, args);
     }
 
-    @Bean
-    CommandLineRunner commandLineRunner(BillRepository  billRepository,
-                                        ProductItemRepository productItemRepository,
-                                        CustomerRestClient customerRestClient,
-                                        ProductRestClient productRestClient){
-
-        return args -> {
-            Collection<Customer> customers = customerRestClient.getAllCustomers().getContent();
-            Collection<Product> products = productRestClient.getAllProducts().getContent();
-
-            customers.forEach(customer -> {
-                Bill bill = Bill.builder()
-                        .billingDate(new Date())
-                        .customerId(customer.getId())
-                        .build();
-                billRepository.save(bill);
-                products.forEach(product -> {
-                    ProductItem productItem = ProductItem.builder()
-                            .bill(bill)
-                            .productId(product.getId())
-                            .quantity(1+new Random().nextInt(10))
-                            .unitPrice(product.getPrice())
-                            .build();
-                    productItemRepository.save(productItem);
-                });
-            });
-        };
-    }
+//    @Bean
+//    CommandLineRunner commandLineRunner(BillRepository  billRepository,
+//                                        ProductItemRepository productItemRepository,
+//                                        CustomerRestClient customerRestClient,
+//                                        ProductRestClient productRestClient){
+//
+//        return args -> {
+//            Collection<Customer> customers = customerRestClient.getAllCustomers().getContent();
+//            Collection<Product> products = productRestClient.getAllProducts().getContent();
+//
+//            customers.forEach(customer -> {
+//                Bill bill = Bill.builder()
+//                        .billingDate(new Date())
+//                        .customerId(customer.getId())
+//                        .build();
+//                billRepository.save(bill);
+//                products.forEach(product -> {
+//                    ProductItem productItem = ProductItem.builder()
+//                            .bill(bill)
+//                            .productId(product.getId())
+//                            .quantity(1+new Random().nextInt(10))
+//                            .unitPrice(product.getPrice())
+//                            .build();
+//                    productItemRepository.save(productItem);
+//                });
+//            });
+//        };
+//    }
 
 }
